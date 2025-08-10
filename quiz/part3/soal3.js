4ebc262d711d10736ec46d9947ef08372b5125b6
WaterGiovannni. <149610292+FahriGio@users.noreply.github.com>
2025-08-10 13:40:19 +0700

    Update soal3.js

--- quiz/part3/soal3.js
+++ quiz/part3/soal3.js
@@ -7,7 +7,7 @@
 // Apabila angka counter adalah angka genap, tuliskan GENAP
 // Apabila angka counter adalah angka ganjil, tuliskan GANJIL
 
-for (let i = 0; i <= 100; i++){
+for (let i = 1 i <= 100; i++){
      if (i % 2 === 0) {
         console.log( i + " " + "genap");
      }
